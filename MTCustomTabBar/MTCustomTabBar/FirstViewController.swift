3bdf1e7fc4b1e110ef1cbce4f2767c943093787b
Murilo Teixeira <murilogteixeira@gmail.com>
2019-08-06 16:50:08 -0300

    Adicionar o projeto inicial para a framework

---
+++ MTCustomTabBar/MTCustomTabBar/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MTCustomTabBar
+//
+//  Created by Murilo Teixeira on 06/08/19.
+//  Copyright © 2019 Murilo Teixeira. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MTCustomTabBar/MTCustomTabBar/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MTCustomTabBar
+//
+//  Created by Murilo Teixeira on 06/08/19.
+//  Copyright © 2019 Murilo Teixeira. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
